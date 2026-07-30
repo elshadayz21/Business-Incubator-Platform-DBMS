4a0c160934218f61c1e11d85052d77910e20c397
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-07-30 11:20:39 +0300

    refactor: completely remove Electron Desktop application

--- Business-Incubator-DB-System/Desktop/src/electron/backend/models/user.model.js
+++
@@ -1,8 +0,0 @@
-import pool from "../config/database.cjs";
-
-export const findUserByEmail = async (email) => {
-  const query = "SELECT * FROM users WHERE email = $1";
-  const values = [email];
-  const result = await pool.query(query, values);
-  return result.rows[0];
-};
