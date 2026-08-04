db722d5b0b9f86dad108e31b986d81804cf59718
Tikyme <tikifantahun@gmail.com>
2026-08-05 01:46:52 +0300

    feat: add TF-IDF ML mentor matcher and assignment UI

--- Website/admin-ui/vite.config.js
+++ Website/admin-ui/vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',
-  build: {
-    outDir: '../public/admin',
-    emptyOutDir: true,
-  },
   server: {
-    port: 5173,
-    strictPort: true,
-  }
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/v1': 'http://localhost:3000',
+    },
+  },
 })
--- Website/routes/projects/project.js
+++ Website/routes/projects/project.js
@@ -4,17 +4,20 @@ import {
   newProjectPage,
   projectsController,
   projectDetailController,
+  getSuggestedMentorsController, // <-- Add this!
 } from "../../controllers/project/project.controller.js";
 import { isAuth } from "../../middleware/auth.middlware.js";
 
 const router = Router();
 
 router.get("/", projectsController);
-
 router.get("/new", newProjectPage);
 
+// Add this line right here! ABOVE /:id
+router.get("/:id/suggested-mentors", getSuggestedMentorsController);
+
 router.get("/:id", projectDetailController);
-
 router.post("/create", isAuth, createProjectController);
-
 export default router;
+
+
